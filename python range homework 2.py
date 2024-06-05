54744e6a855261427bccf96f57fee758a759af64
JustinChere01 <justinchere05@gmail.com>
2024-06-05 00:13:46 -0400

    Add files via upload

---
+++ python range homework 2.py
@@ -0,0 +1,15 @@
+while True:
+  answer=input(' what is your favorite flavor of ice cream ?')
+  if answer == "chocolate":
+    print("correct answer")
+    break
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 5:
+    break
+  i += 1
+    
+
+
---
+++ python range homework.py
@@ -0,0 +1,4 @@
+days_of_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+for index in range(len(days_of_week)):
+    if index %2 == 0:
+        print(index , days_of_week[index] )
